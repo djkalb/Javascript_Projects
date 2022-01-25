59f31d9a3fd040d166177060a4058259fc276e3b
djkalb <71739290+djkalb@users.noreply.github.com>
2022-01-25 13:53:19 -0800

    Project 7 submission

---
+++ Basic_Javascript_Projects/Project7_scope_time_function”/js/main.js
@@ -0,0 +1,23 @@
+function greetMe () {
+    if (new Date().getHours() < 11 ) {
+        document.getElementById("greeting").innerHTML = "good morning"
+    }
+    else if (new Date().getHours() > 19 ){
+        document.getElementById("greeting").innerHTML = "good night"
+    }
+    else {
+        document.getElementById("greeting").innerHTML = "good afternoon"
+    }
+}
+function ageCheck() {
+    let age = document.getElementById("ager").value;
+    console.log(age);
+    if( age < 100) {
+        document.getElementById("answer").innerHTML = 
+        "You are not that old yet"
+    }
+    else {
+        document.getElementById("answer").innerHTML = 
+        "You are definitely old"
+    }
+}
