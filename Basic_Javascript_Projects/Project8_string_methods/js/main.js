15c35e1f77182d2c91312dc1072f374cdfe4146c
djkalb <71739290+djkalb@users.noreply.github.com>
2022-01-25 14:56:27 -0800

    project 8 submission

---
+++ Basic_Javascript_Projects/Project8_string_methods/js/main.js
@@ -0,0 +1,31 @@
+
+function addStrings() {
+    let a = "asdf"
+let b = "zxcv"
+let c = "qwer"
+document.getElementById("asdf").innerHTML =  a.concat(b, c);
+
+}
+function slicer() {
+    let letters = "carts go behind horses"
+    document.getElementById("return").innerHTML = 
+    letters.slice(0, 5).toUpperCase();
+}
+function searcher() {
+    let test = "heeps of fun";
+    document.getElementById("search").innerHTML = test.search("haystack")
+}
+let numbers = 123;
+function stringer (n) {
+    return n.toString();
+}
+function preciser(n) {
+    return n.toPrecision(4);
+}
+
+
+document.write(stringer(numbers));
+document.write(preciser(1672348));
+
+let text = "hello world"
+document.write(text.valueOf());
