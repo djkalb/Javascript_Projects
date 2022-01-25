5c0bc0db16230d883bc3f5b585f6ac61210dc490
djkalb <71739290+djkalb@users.noreply.github.com>
2022-01-24 19:29:19 -0800

    Project 5

---
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,34 @@
+document.write(typeof 11)   
+
+document.write(isNaN("string"))
+document.write(isNaN(15))
+
+
+
+
+function nan () {
+    document.getElementById("asdf").innerHTML = 0/0;
+}
+function infine () {
+    document.getElementById("infinity").innerHTML = 2E350
+}
+function nInfine () {
+    document.getElementById("nInfinity").innerHTML = -2E350
+}
+
+document.write(5 > 5);
+
+console.log(3 + 0);
+console.log(3 < 0);
+console.log(3 == 0);
+console.log(3 == 3);
+console.log(3 === 0);
+console.log(3 === 3);
+console.log(3 === "3");
+console.log(3 > 0 && 1 > 0);
+console.log(3 > 0 && 1 > 3);
+console.log(3 > 0 || 1 > 5);
+console.log(3 > 5 || 1 > 5);
+console.log(!(3 == 3));
+console.log(!(3 < 3));
+
